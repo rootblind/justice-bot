b8a43a42cd1e544c0c8a189675d4cb71b0971759
rootblind <blindcatalin@protonmail.com>
2025-07-01 16:19:09 +0300

    changed logging flagged messages

--- Events/Guild/messageCreate.js
+++ Events/Guild/messageCreate.js
@@ -137,7 +137,7 @@ module.exports = {
                     .setFooter({text:`ID: ${message.author.id}`})
                 
                 if(message.content.length <= 3000)
-                    embed.setDescription(`**Content**: ${response['text']}`)
+                    embed.setDescription(`**Content**: ${message.content}`)
                 else { // handling large messages through temp files and posting the file instead of overflowing the embed description
                     const filePath = path.join(__dirname, `../../temp/${message.id}.txt`);
                     fs.writeFile(filePath, message.content, (err) => {
